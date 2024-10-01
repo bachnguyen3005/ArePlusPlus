90f6488738f0053dc1b687dfe24e25b34a7e4f38
deltaqyto <deltaqyto@gmail.com>
2024-10-01 13:14:56 +1000

    driving controls

--- src/taskhandler.cpp
+++ src/taskhandler.cpp
@@ -8,6 +8,9 @@
 #include "nav_msgs/msg/odometry.hpp"
 #include "std_msgs/msg/int32_multi_array.hpp"
 #include "geometry_msgs/msg/pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp" 
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
 
 #include "ament_index_cpp/get_package_share_directory.hpp"
 #include "ament_index_cpp/get_package_prefix.hpp"
@@ -25,6 +28,9 @@
 // and monitor to see if it stalls/reaches. If stalled, abort and go to the next task (log an error)
 // If reached, try to get the next waypoint. If no waypoints remain, log sucessful completion and then load the next task.
 
+using NavigateToPose = nav2_msgs::action::NavigateToPose;
+using GoalHandleNavigateToPose = rclcpp_action::ClientGoalHandle<NavigateToPose>;
+
 class TaskPlanner : public rclcpp::Node {
 public:
     TaskPlanner(std::vector<std::pair<int, int>> initial_tasks) : Node("task_planner") {
@@ -33,6 +39,15 @@ public:
 
         move_orders_sub_ = this->create_subscription<std_msgs::msg::Int32MultiArray>(
             "/move_orders", 10, std::bind(&TaskPlanner::move_orders_callback, this, std::placeholders::_1));
+        
+        nav_to_pose_client_ = rclcpp_action::create_client<NavigateToPose>(
+            this, "navigate_to_pose");
+
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(500),
+            std::bind(&TaskPlanner::try_assign_task, this));
+
+
         for (auto task: initial_tasks){
             order_queue.push(task);
         }
@@ -47,6 +62,17 @@ public:
             "/move_orders", 10, std::bind(&TaskPlanner::move_orders_callback, this, std::placeholders::_1));
     }
 
+    void goal_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
+        RCLCPP_INFO(this->get_logger(), "Received goal pose: x=%f, y=%f, z=%f, orientation: x=%f, y=%f, z=%f, w=%f",
+                    msg->pose.position.x,
+                    msg->pose.position.y,
+                    msg->pose.position.z,
+                    msg->pose.orientation.x,
+                    msg->pose.orientation.y,
+                    msg->pose.orientation.z,
+                    msg->pose.orientation.w);
+    }
+
     // Set a product location
     void set_product_location(int id, float x, float y, float yaw) {
         product_locations[id] = Pose2d(x, y, yaw);
@@ -55,6 +81,7 @@ public:
     // Set all product locations 
     void set_all_product_locations(const std::unordered_map<int, Pose2d>& new_locations) {
         product_locations = new_locations;
+        RCLCPP_INFO(this->get_logger(), "Updated product locations. New count: %zu", product_locations.size());
     }
 
     // Get a product location
@@ -124,14 +151,43 @@ public:
 
     void set_activation_state(bool state) {
         activate = state;
+        // Todo respect activation state, autoqueue
     }
 
 private:
+    void send_movement_command(const geometry_msgs::msg::PoseStamped& target_pose) {
+        // Wait for action server
+        while (!nav_to_pose_client_->wait_for_action_server(std::chrono::seconds(1))) {
+            if (!rclcpp::ok()) {
+                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for action server.");
+                return;
+            }
+            RCLCPP_INFO(this->get_logger(), "Waiting for action server...");
+        }
+
+        // Create goal
+        auto goal_msg = NavigateToPose::Goal();
+        goal_msg.pose = target_pose;
+
+        // Send goal
+        auto send_goal_options = rclcpp_action::Client<NavigateToPose>::SendGoalOptions();
+        send_goal_options.goal_response_callback =
+            std::bind(&TaskPlanner::goal_response_callback, this, std::placeholders::_1);
+        send_goal_options.feedback_callback =
+            std::bind(&TaskPlanner::feedback_callback, this, std::placeholders::_1, std::placeholders::_2);
+        send_goal_options.result_callback =
+            std::bind(&TaskPlanner::result_callback, this, std::placeholders::_1);
+
+        nav_to_pose_client_->async_send_goal(goal_msg, send_goal_options);
+        RCLCPP_INFO(this->get_logger(), "Sent movement command to navstack: x=%f, y=%f",
+                    target_pose.pose.position.x, target_pose.pose.position.y);
+    }
+
     void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-        RCLCPP_INFO(this->get_logger(), "Received odom: x=%f, y=%f, z=%f",
-                    msg->pose.pose.position.x,
-                    msg->pose.pose.position.y,
-                    msg->pose.pose.position.z);
+        //RCLCPP_INFO(this->get_logger(), "Received odom: x=%f, y=%f, z=%f",
+        //            msg->pose.pose.position.x,
+        //            msg->pose.pose.position.y,
+        //            msg->pose.pose.position.z);
     }
 
     void move_orders_callback(const std_msgs::msg::Int32MultiArray::SharedPtr msg) {
@@ -148,14 +204,92 @@ private:
         }
     }
 
+    void feedback_callback(
+        GoalHandleNavigateToPose::SharedPtr,
+        const std::shared_ptr<const NavigateToPose::Feedback> feedback) {
+        //RCLCPP_INFO(this->get_logger(), "Distance remaining: %f", feedback->distance_remaining);
+    }
+
+    void goal_response_callback(const GoalHandleNavigateToPose::SharedPtr & goal_handle) {
+        if (!goal_handle) {
+            RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    void result_callback(const GoalHandleNavigateToPose::WrappedResult & result) {
+        switch (result.code) {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+                ready_for_task = true;
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+                ready_for_task = true;
+                break;
+            default:
+                RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+                break;
+        }
+    }
+
+    void try_assign_task(){
+        if (!activate) {return;}
+        if (!ready_for_task) {return;}
+        if (!has_orders()) {return;}
+        transmit_task();
+    }
+
+    void transmit_task() {
+        if (!has_orders()) {
+            RCLCPP_WARN(this->get_logger(), "No tasks in queue to process");
+            return;
+        }
+
+        auto task = order_queue.front();
+        order_queue.pop();
+
+        auto product_location = get_product_location(task.first);
+        
+        geometry_msgs::msg::PoseStamped target_pose;
+        target_pose.header.stamp = this->now();
+        target_pose.header.frame_id = "map";
+        target_pose.pose.position.x = product_location.pos_x;
+        target_pose.pose.position.y = product_location.pos_y;
+        target_pose.pose.position.z = 0.0;
+        
+
+        double cy = std::cos(product_location.yaw * 0.5);
+        double sy = std::sin(product_location.yaw * 0.5);
+        target_pose.pose.orientation.x = 0.0;
+        target_pose.pose.orientation.y = 0.0;
+        target_pose.pose.orientation.z = sy;
+        target_pose.pose.orientation.w = cy;
+        ready_for_task = false;
+        send_movement_command(target_pose);
+        RCLCPP_INFO(this->get_logger(), "Sent movement command for task: product %d to station %d", 
+                    task.first, task.second);
+        
+    }
+
     std::unordered_map<int, Pose2d> product_locations;
     std::unordered_map<int, Pose2d> station_locations;
     std::queue<std::pair<int, int>> order_queue;
     
     bool activate = false;
+    bool ready_for_task = true;
     Pose2d current_waypoint = Pose2d(0, 0, 0);
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr move_orders_sub_;
+    
+    rclcpp_action::Client<NavigateToPose>::SharedPtr nav_to_pose_client_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+
 };
 
 int main(int argc, char* argv[]) {
@@ -179,9 +313,9 @@ int main(int argc, char* argv[]) {
         std::unordered_map<int, Pose2d> station_locations;
         std::unordered_map<int, Pose2d> product_locations;
         
-        station_locations[1] = Pose2d(1, 5, 0);
-        station_locations[2] = Pose2d(3, 5, 0);
-        station_locations[3] = Pose2d(5, 5, 0);
+        station_locations[1] = Pose2d(1, 1, 0);
+        station_locations[2] = Pose2d(1, 1, 0);
+        station_locations[3] = Pose2d(5, 1, 0);
 
         station_locations[-1] = Pose2d(1, 3, 0);  // TODO -stations are for products to stay
         station_locations[-2] = Pose2d(3, 3, 0);
@@ -190,15 +324,16 @@ int main(int argc, char* argv[]) {
         station_locations[-5] = Pose2d(3, 0, 0);
         station_locations[-6] = Pose2d(5, 0, 0);
 
-        product_locations[1] = Pose2d(1, 3, 0);
-        product_locations[2] = Pose2d(3, 3, 0);
-        product_locations[3] = Pose2d(5, 3, 0);
+        product_locations[1] = Pose2d(0.5, -1, 0);
+        product_locations[2] = Pose2d(1.0, -1, 0);
+        product_locations[3] = Pose2d(1.5, -1, 0);
         product_locations[4] = Pose2d(1, 0, 0);
         product_locations[5] = Pose2d(3, 0, 0);
         product_locations[6] = Pose2d(5, 0, 0);
 
         node->set_all_product_locations(product_locations);
         node->set_all_station_locations(station_locations);
+        node->set_activation_state(true);
     }
 
     rclcpp::spin(node);
